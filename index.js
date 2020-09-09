da1d8a6d83f494bbbc1ac9885389c16a34f17aec
Zaigham-Javed <zaigham.javed@ideofuzion.com>
2020-09-09 16:27:21 +0500

    remove recursion in S3storage

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-'use strict';
+'use strict'
 
-const Server = require('./lib/Server');
-const DataStore = require('./lib/stores/DataStore');
-const FileStore = require('./lib/stores/FileStore');
-const GCSDataStore = require('./lib/stores/GCSDataStore');
-const S3Store = require('./lib/stores/S3Store');
-const ERRORS = require('./lib/constants').ERRORS;
-const EVENTS = require('./lib/constants').EVENTS;
-console.log('its me')
+const Server = require('./lib/Server')
+const DataStore = require('./lib/stores/DataStore')
+const FileStore = require('./lib/stores/FileStore')
+const GCSDataStore = require('./lib/stores/GCSDataStore')
+const S3Store = require('./lib/stores/S3Store')
+const ERRORS = require('./lib/constants').ERRORS
+const EVENTS = require('./lib/constants').EVENTS
 module.exports = {
     Server,
     DataStore,
@@ -16,4 +15,4 @@ module.exports = {
     S3Store,
     ERRORS,
     EVENTS,
-};
+}
