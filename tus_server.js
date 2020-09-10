49d066a505eb7dc3906e63e2c7ca6ed451bd71d9
talha883 <talhamalik883@gmail.com>
2020-09-11 02:13:45 +0500

    fix bucket name issue

---
+++ tus_server.js
@@ -0,0 +1,20 @@
+const tus = require('./index')
+const AWS = require('aws-sdk')
+const ep = new AWS.Endpoint('s3.wasabisys.com')
+const server = new tus.Server();
+server.datastore = new tus.S3Store({
+  path: '/',
+  endpoint: ep,
+  bucket: 'userdata',
+  accessKeyId: 'N84PSPGUVR6ZPOSZERZ5',
+  secretAccessKey: 'q46DOWhF2zeI4SqRh4RHlZF0NBOmzve6yeQPn46n',
+  region: 'us-east-1',
+  partSize: 8 * 1024 * 1024, // each uploaded part will have ~8MB,
+  tmpDirPrefix: 'tus-s3-store',
+})
+
+const host = '127.0.0.1';
+const port = 1080;
+server.listen({ host, port }, () => {
+    console.log(`[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`);
+});
