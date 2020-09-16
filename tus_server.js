4270c9546515a9e90dbcf5406803cdeaaa8658f6
Zaigham-Javed <zaigham.javed@ideofuzion.com>
2020-09-16 18:43:52 +0500

    upload file done

--- tus_server.js
+++ tus_server.js
@@ -1,20 +1,23 @@
 const tus = require('./index')
 const AWS = require('aws-sdk')
+const EVENTS = require('./index').EVENTS
 const ep = new AWS.Endpoint('s3.wasabisys.com')
-const server = new tus.Server();
+const server = new tus.Server()
 server.datastore = new tus.S3Store({
-  path: '/',
-  endpoint: ep,
-  bucket: 'userdata',
-  accessKeyId: 'N84PSPGUVR6ZPOSZERZ5',
-  secretAccessKey: 'q46DOWhF2zeI4SqRh4RHlZF0NBOmzve6yeQPn46n',
-  region: 'us-east-1',
-  partSize: 8 * 1024 * 1024, // each uploaded part will have ~8MB,
-  tmpDirPrefix: 'tus-s3-store',
+    path: '/',
+    endpoint: ep,
+    bucket: 'userdata',
+    accessKeyId: 'N84PSPGUVR6ZPOSZERZ5',
+    secretAccessKey: 'q46DOWhF2zeI4SqRh4RHlZF0NBOmzve6yeQPn46n',
+    region: 'us-east-1',
+    partSize: 8 * 1024 * 1024, // each uploaded part will have ~8MB,
+    tmpDirPrefix: 'tus-s3-store',
 })
 
-const host = '127.0.0.1';
-const port = 1080;
+const host = '127.0.0.1'
+const port = 1080
 server.listen({ host, port }, () => {
-    console.log(`[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`);
-});
+    console.log(
+        `[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`
+    )
+})
