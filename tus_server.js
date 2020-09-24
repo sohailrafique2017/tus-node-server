77bff4f0ff95d389128923edb48c8b22c9643297
zaigham javed <zaigham.javed@ideofuzion.com>
2020-09-24 11:56:31 +0500

    fix issues

--- tus_server.js
+++ tus_server.js
@@ -4,20 +4,20 @@ const EVENTS = require('./index').EVENTS
 const ep = new AWS.Endpoint('s3.wasabisys.com')
 const server = new tus.Server()
 server.datastore = new tus.S3Store({
-    path: '/',
-    endpoint: ep,
-    bucket: 'userdata',
-    accessKeyId: 'N84PSPGUVR6ZPOSZERZ5',
-    secretAccessKey: 'q46DOWhF2zeI4SqRh4RHlZF0NBOmzve6yeQPn46n',
-    region: 'us-east-1',
-    partSize: 8 * 1024 * 1024, // each uploaded part will have ~8MB,
-    tmpDirPrefix: 'tus-s3-store',
+  path: '/',
+  endpoint: ep,
+  bucket: 'userdata',
+  accessKeyId: 'N84PSPGUVR6ZPOSZERZ5',
+  secretAccessKey: 'q46DOWhF2zeI4SqRh4RHlZF0NBOmzve6yeQPn46n',
+  region: 'us-east-1',
+  partSize: 8 * 1024 * 1024, // each uploaded part will have ~8MB,
+  tmpDirPrefix: 'tus-s3-store',
 })
 
-const host = '127.0.0.1'
+const host = '192.168.1.29'
 const port = 1080
 server.listen({ host, port }, () => {
-    console.log(
-        `[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`
-    )
+  console.log(
+    `[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`
+  )
 })
